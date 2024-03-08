cf0cc1ce9999c69d3b6b58809d6633122844eed5
Henrique Andrade <henriqueandradee10@hotmail.com>
2024-03-08 16:43:31 -0300

    Projeto completo

---
+++ index.js
@@ -0,0 +1,30 @@
+class Hero {
+    constructor(name, age, type){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+
+    }
+
+    toAttack(){
+
+        let attack;
+
+        if (this.type === "warrior"){
+            attack = "sword"
+        } else if (this.type === "mage"){
+            attack = "magic"
+        } else if (this.type === "monk"){
+            attack = "martial arts"
+        } else if (this.type === "ninja"){
+            attack = "shuriken"
+        }
+        
+            console.log(`${this.name} ${this.age} years old of the ${this.type} attacked using ${attack}`)
+
+    }
+
+}
+
+    hero.toAttack()
+    let hero = new hero(prompt("enter your name"),prompt("enter your age"),prompt("enter your type"))
